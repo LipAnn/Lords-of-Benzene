7df54c41f72f832e3be2a91b68a84cfc1ee19a87
kozyablo <kozyablo.dev@gmail.com>
2015-11-11 19:28:29 +0300

    CommonHeader was made even more powerful!

--- CommonHeader.h
+++ CommonHeader.h
@@ -1,4 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <memory.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <vector>
+
+class ImagePack_t 
+{
+  public:
+    vector<cv::Mat> sample_pack;
+    vector<bool> sample_info;
+    
+    int size ();
+    bool AddImage (cv::Mat image, bool image_info = false);
+    bool DeleteImage (int index);
+    cv::Mat operator[] (int index);
+};
+
+bool ImagePack_t::AddImage(cv::Mat image, bool image_info)
+{
+}
+
+bool ImagePack_t::DeleteImage(int index)
+{
+}
+
+cv::Mat ImagePack_t::operator[](int index)
+{
+    return sample_pack[index];
+}
+
+int ImagePack_t::size()
+{
+    return sample_pack.size();
+}
